b56ca3bb656d35d9188690394219781c254d5f0b
Truong <nhattruongtp2000@gmail.com>
2020-12-12 12:43:26 +0700

    them dang ky

---
+++ update-register/WebAPI.Data/Entities/productSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WebAPI.Data.Entities
+{
+    public class productSize
+    {
+        [Required]
+        [Column(TypeName = "VARCHAR(200)")]
+        public string idSize { get; set; }
+        [Required]
+        [Column(TypeName = "VARCHAR(200)")]
+        public string sizeName { get; set; }
+        public virtual ICollection<products> Products { get; set; }
+        public string LanguageId { set; get; }
+
+        public Language Language { get; set; }
+    }
+}
