aa7ddf712c8913e7966a2fcb946a6ce7629acfa6
Truong <nhattruongtp2000@gmail.com>
2020-12-13 13:01:13 +0700

    add cart to project

---
+++ Add-Cart/WebAPI.Application/Catalog/Categories/ICategoryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.ViewModels.Catalog.Categories;
+using WebAPI.ViewModels.Common;
+
+namespace WebAPI.Application.Catalog.Categories
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryVm>> GetAll(string languageId);
+
+        Task<PagedResult<CategoryVm>> GetCategoriesPagings(GetCategoryPagingRequest request);
+
+        Task<int> Create(CategoryCreateRequest request);
+
+        Task<CategoryVm> GetById( string languageId, int categoryId);
+
+        Task<int> Update(CategoryUpdateRequest request);
+
+        Task<int> Delete(int idCategory);
+
+
+    }
+}
---
+++ Add-Cart/WebAPI.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.Data.Entities;
+using WebAPI.Data.Enums;
+
+namespace WebAPI.Data.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<productDetail>().HasData(
+                new productDetail() { ProductId = 1,idProductDetail=1, LanguageId = "vi", ProductName="Shoe",price=1000000,salePrice=1000000,detail="goood product",isSaling=false,dateAdded=new DateTime(2019,10,21)},
+                new productDetail() { ProductId = 2,idProductDetail = 2, LanguageId = "vi", ProductName = "Pro" ,price = 2000000, salePrice = 1000000, detail = "goood product", isSaling = false, expiredSalingDate=new DateTime(2020,10,12), dateAdded = new DateTime(2019, 10, 21) }
+            );;
+            modelBuilder.Entity<Language>().HasData(
+                new Language() { Id = "vi", Name = "Tiếng Việt", IsDefault = true },
+                new Language() { Id = "en", Name = "English", IsDefault = false }
+            );
+
+            modelBuilder.Entity<productSize>().HasData(
+                new productSize() { idSize="1",sizeName="L"},
+                new productSize() { idSize="2",sizeName="M"}
+            );
+            modelBuilder.Entity<productBrand>().HasData(
+                new productBrand() { idBrand="1",brandName="Logo",brandDetail="Logo"},
+                new productBrand() { idBrand = "2", brandName = "Company", brandDetail = "Company" }
+            );
+            modelBuilder.Entity<productColor>().HasData(
+                new productColor() { idColor="ffffff",colorName="While"},
+                new productColor() { idColor = "Red", colorName = "Red" }
+            );
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { idCategory=1, IsShowOnHome = true, SortOrder = 1, },
+                new Category() { idCategory = 2, IsShowOnHome = true, SortOrder = 2, }
+            );
+            modelBuilder.Entity<productTypes>().HasData(
+                new productTypes() { idType="1",typeName="Cheap"},
+                new productTypes() { idType = "2", typeName = "Expensive" }
+            );
+            modelBuilder.Entity<products>().HasData(
+                new products() { ProductId = 1 ,idSize = "1", idBrand = "1", idColor = "ffffff",  idType = "1" },
+                new products() { ProductId = 2, idSize = "1", idBrand = "1", idColor = "ffffff",  idType = "1" }
+            );
+            modelBuilder.Entity<ProductInCategory>().HasData(
+                new ProductInCategory() { ProductId = 1, idCategory = 1 }
+                
+                );
+            modelBuilder.Entity<CategoryTranslation>().HasData(
+                 new CategoryTranslation() { Id = 1, CategoryId = 1, Name = "Áo nam", LanguageId = "vi", SeoAlias = "ao-nam", SeoDescription = "Sản phẩm áo thời trang nam", SeoTitle = "Sản phẩm áo thời trang nam" },
+                 new CategoryTranslation() { Id = 2, CategoryId = 1, Name = "Men Shirt", LanguageId = "en", SeoAlias = "men-shirt", SeoDescription = "The shirt products for men", SeoTitle = "The shirt products for men" },
+                 new CategoryTranslation() { Id = 3, CategoryId = 2, Name = "Áo nữ", LanguageId = "vi", SeoAlias = "ao-nu", SeoDescription = "Sản phẩm áo thời trang nữ", SeoTitle = "Sản phẩm áo thời trang women" },
+                 new CategoryTranslation() { Id = 4, CategoryId = 2, Name = "Women Shirt", LanguageId = "en", SeoAlias = "women-shirt", SeoDescription = "The shirt products for women", SeoTitle = "The shirt products for women" }
+                   );
+
+            // any guid
+
+            var roleId = new Guid("8D04DCE2-969A-435D-BBA4-DF3F325983DC");
+            var adminId = new Guid("69BD714F-9576-45BA-B5B7-F00649BE00DE");
+            modelBuilder.Entity<role>().HasData(new role
+            {
+                Id = roleId,
+                Name = "admin",
+                NormalizedName = "admin",
+                Description = "Administrator role"
+            });
+
+            var hasher = new PasswordHasher<users>();
+            modelBuilder.Entity<users>().HasData(new users
+            {
+                Id = adminId,
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                Email = "nhattruongtp2000@gmail.com",
+                NormalizedEmail = "nhattruongtp2000@gmail.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Admin@123"),
+                SecurityStamp = string.Empty,
+                birthday=new DateTime(2020,10,12),
+                firstName = "Nguyen",
+                lastName = "Truong",
+                lastLogin=new DateTime(2020,11,13)
+ 
+            });
+
+            modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(new IdentityUserRole<Guid>
+            {
+                RoleId = roleId,
+                UserId = adminId
+            });
+
+            modelBuilder.Entity<Slide>().HasData(
+              new Slide() { Id = 1, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 1, Url = "#", Image = "/themes/images/carousel/1.png", Status = true },
+              new Slide() { Id = 2, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 2, Url = "#", Image = "/themes/images/carousel/2.png", Status = true },
+              new Slide() { Id = 3, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 3, Url = "#", Image = "/themes/images/carousel/3.png", Status = true },
+              new Slide() { Id = 4, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 4, Url = "#", Image = "/themes/images/carousel/4.png", Status = true },
+              new Slide() { Id = 5, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 5, Url = "#", Image = "/themes/images/carousel/5.png", Status = true },
+              new Slide() { Id = 6, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 6, Url = "#", Image = "/themes/images/carousel/6.png", Status = true }
+              );
+
+        }
+    }
+}
