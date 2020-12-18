de8acf231b916718998a9f072569a2d292875496
Truong <nhattruongtp2000@gmail.com>
2020-12-18 22:04:05 +0700

    hoan thanh checkout user va gio hang

---
+++ finish-cart/WebAPI.Application/Catalog/Orders/IOrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Data.Entities;
+using WebAPI.Models;
+using WebAPI.ViewModels.Catalog.Products;
+using WebAPI.ViewModels.Common;
+using WebAPI.ViewModels.Orders;
+using WebAPI.ViewModels.Sales;
+
+namespace WebAPI.Application.Catalog.Orders
+{
+    public interface IOrderService
+    {
+        Task<List<OrderVm>> GetAll(string languageId);
+
+        Task<List<OrderVm>> GetAllByUser(string User, string languageId);
+
+        Task<PagedResult<OrderVm>> GetOrdersPagings(GetOrderPagingRequest request);
+
+        Task<int> Create(CheckoutRequest request);
+
+        Task<OrderVm> GetById(int id, string languageId);
+
+        Task<int> Delete(int id);
+
+      
+    }
+}
