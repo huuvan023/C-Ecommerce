029dbc38c41f6023bf30573e09613fe14e2ac20c
Ngo Huu Van <huu.van.23tg@gmail.com>
2020-12-24 14:19:16 +0700

    order list

---
+++ EmmerceAPIHCMUE/Controllers/OrdersListController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using EmmerceAPIHCMUE.Controllers;
+using System.Web;
+using EmmerceAPIHCMUE.Models;
+using EmmerceAPIHCMUE.Provider;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmmerceAPIHCMUE.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("/checkout/")]
+    public class OrdersListController: ControllerBase
+    {
+        [HttpPost("add")]
+        public ResponseData AddCheckout([FromBody] OrdersList orders)
+        {
+            try
+            {
+                if (Request.Headers["Authorization"] != "")
+                {
+                    CustomMiddleware middleware = new CustomMiddleware(Request.Headers["Authorization"]);
+                    if (middleware.ValidateToken(Constants.Instance.USER_ROLE1))
+                    {
+                        if (orders.AddCheckout())
+                        {
+                            return new ResponseData(Constants.Instance.SUCCESS_CODE, Constants.Instance.SUCCESS_MESSAGE1, null);
+                        }
+                        return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SOMETHING_WAS_WRONG, null);
+                    }
+                    else
+                    {
+                        return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SESSION_EXPIRED, null);
+                    }
+                }
+                else
+                {
+                    return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SESSION_EXPIRED, null);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ResponseData(Constants.Instance.FAIL_CODE, e.Message.ToString(), null);
+            }
+        }
+        [HttpPost("update-status")]
+        public ResponseData UpdateStatus([FromBody] OrdersList orders)
+        {
+            try
+            {
+                if (Request.Headers["Authorization"] != "")
+                {
+                    CustomMiddleware middleware = new CustomMiddleware(Request.Headers["Authorization"]);
+                    if (middleware.ValidateToken(Constants.Instance.USER_ROLE1))
+                    {
+                        if (orders.UpdateStatusCheckout())
+                        {
+                            return new ResponseData(Constants.Instance.SUCCESS_CODE, Constants.Instance.SUCCESS_MESSAGE1, null);
+                        }
+                        return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SOMETHING_WAS_WRONG, null);
+                    }
+                    else
+                    {
+                        return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SESSION_EXPIRED, null);
+                    }
+                }
+                else
+                {
+                    return new ResponseData(Constants.Instance.FAIL_CODE, Constants.Instance.SESSION_EXPIRED, null);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ResponseData(Constants.Instance.FAIL_CODE, e.Message.ToString(), null);
+            }
+        }
+    }
+}
---
+++ EmmerceAPIHCMUE/Models/OrdersList.cs
@@ -0,0 +1,69 @@
+﻿using EmmerceAPIHCMUE.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmmerceAPIHCMUE.Models
+{
+    public class OrdersList
+    {
+        private string IdOrderList;
+        private string IdOrder;
+        private string IdUser;
+        private List<MultipleProduct> Products;
+        public string idOrderList { get => IdOrderList; set => IdOrderList = value; }
+        public string idOrder { get => IdOrder; set => IdOrder = value; }
+        public string idUser { get => IdUser; set => IdUser = value; }
+
+        public List<MultipleProduct> products { get => Products; set => Products = value; }
+
+        public bool AddCheckout()
+        {
+            string idOder = Helper.Instance.CreateRandomPassword(10);
+            int totalPrice = 0;
+            string queryinitOrderDetail = "insert into dbo.odersDetails values('" + idOder + "','" + DateTime.UtcNow.ToString() + "','" + Constants.Instance.INITIAL_STT1 + "',0)";
+            if (Connection.Instance.ExecuteNonQuery(queryinitOrderDetail) < 1)
+            {
+                return false;
+            }
+            foreach (MultipleProduct item in this.products)
+            {
+                string query1 = "insert into odersList values('" + Helper.Instance.CreateRandomPassword(6) + "','" + idOder + "','" + this.idUser + "','" + item.idProduct + "', '" + item.quanlity + "')";
+                if (Connection.Instance.ExecuteNonQuery(query1) == 1)
+                {
+                    string queryGetProduct = "select price from dbo.products where idProduct='" + item.idProduct + "'";
+                    string localPrice = (string) Connection.Instance.ExecuteScalar(queryGetProduct);
+                    totalPrice += Int32.Parse(localPrice) * item.quanlity;
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string updateOrderDetail = "update dbo.odersDetails set totalPrice = " + totalPrice + " where idOder = '" + idOder + "'";
+            if (Connection.Instance.ExecuteNonQuery(updateOrderDetail) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool UpdateStatusCheckout()
+        {
+            string query = "update dbo.odersDetails set status = status + 1 where idOder = '"+this.idOrder+"'";
+            if (Connection.Instance.ExecuteNonQuery(query) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public class MultipleProduct
+    {
+        private string IdProduct;
+        private int Quanlity;
+        public string idProduct { get => IdProduct; set => IdProduct = value; }
+        public int quanlity { get => Quanlity; set => Quanlity = value; }
+    }
+}
